f98b975ce95d8d2073e4680a92e7f8fdcdecb0d2
Fr2dX43 <a_a93@live.se>
2022-10-08 20:31:19 +0200

    w6

---
+++ w6.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+#kaptiel fem i python mapen för tidshantering
+from datetime import datetime, timedelta #means from the module "datetime" import the class "datetime" and class timedelta
+
+now = datetime.now() #collect the current time
+
+print(now) #prints call opon now which shows the current time
