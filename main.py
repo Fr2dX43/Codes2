fa7894a19535a60df7d88214f5c3688b605acf41
Fr2dX43 <a_a93@live.se>
2022-10-23 23:01:37 +0200

    5

--- main.py
+++ main.py
@@ -24,22 +24,58 @@
 
 # konstoktor __init__
 import os
+from turtle import color
 os.system("cls")
-class Student:
-    def __init__(self, namn:str, age:int):
-        #skapa alla propertties/Engenskaper/varibler
-        #som hör till ritningen
-        self.Namn = namn 
-        #self.Age = age
-        self.__Age = age #priviate/protected variabel 
-        self.Kurslista = []
-
-stefan = Student("Stefan", -12) #skapar en student
-
-stefan.Age = -123 #för att sätta värde
-stefan.__Age = -34
-print(stefan.__Age)
-print(stefan.Namn, stefan.Age)
+############exemple på OOP##################
+class Ritning:
+    def __init__(self):
+        self.Color = ""
+    def Repaint(self, color:str):
+        self.__Color = color
+    def GetColor(self):
+        return self.__Color
 
 
 
+ayadsHus = Ritning()
+ayadsHus.Repaint("blue")
+annanHus = Ritning()
+annanHus.Repaint("Red")
+print(ayadsHus.GetColor())
+print(annanHus.GetColor())
+##########################################
+
+
+
+# class Student:
+#     def __init__(self, namn:str, age:int):
+#         #skapa alla propertties/Engenskaper/varibler
+#         #som hör till ritningen
+#         self.Namn = namn 
+#         #self.Age = age
+#         self.__Age = age #priviate/protected variabel
+#         self.__postnr = "" #properties bäst och köra __ på allt
+#         self.Kurslista = []
+
+#     def SetAge(self, newAge:int): #för att ändra på __Age
+#         if newAge > 0:
+#             self.__Age = newAge
+
+# stefan = Student("Stefan", -12) #skapar en student
+# stefan.SetAge(-13)
+# stefan.SetAge(132)
+# #print(stefan.__Age)
+# stefan.Age = -123 #för att sätta värde
+
+# print(stefan.Namn, stefan.Age)
+# #print(stefan.__dict__) #varje object ligger som dict
+
+# #Guide för att skapa en class
+# # 1. class Namn
+# # 2. def__init__(self)
+# #         här skapar du properties för objecktet
+# # 3. __ på alla propeties
+# # 4. getter och setters med Vaildering
+# # 5. constuctor - vad skicka in
+
+# #lektion 1:24 min
