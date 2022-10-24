767e555848eb22ac3c6a0a212b6d85bf3f568fe2
Fr2dX43 <a_a93@live.se>
2022-10-24 21:49:16 +0200

    w6

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+
 #class är en mall dvs bara en ritnig
-#object utefrån class 
+#object utefrån class - object är en instans av en class
 
 ########Exempel på hur man inte ska göra########
 #dålig object
@@ -23,52 +24,74 @@
 #innåt i en object (priviate/protected variabel)
 
 # konstoktor __init__
+# import os
+# from turtle import color
+# os.system("cls")
+# ############exemple på OOP##################
+# class Ritning:
+#     def __init__(self):
+#         self.Color = ""
+#     def Repaint(self, color:str):
+#         self.__Color = color
+#     def GetColor(self):
+#         return self.__Color
+
+
+
+# ayadsHus = Ritning()
+# ayadsHus.Repaint("blue")
+# annanHus = Ritning()
+# annanHus.Repaint("Red")
+# print(ayadsHus.GetColor())
+# print(annanHus.GetColor())
+##########################################
 import os
-from turtle import color
 os.system("cls")
-############exemple på OOP##################
+
+#self används är för att särskilja olika objeckt
+
 class Ritning:
-    def __init__(self):
+    def __init__(self): #metod
         self.Color = ""
-    def Repaint(self, color:str):
+    def Repaint(self,color:str):
         self.__Color = color
     def GetColor(self):
         return self.__Color
 
 
 
-ayadsHus = Ritning()
-ayadsHus.Repaint("blue")
-annanHus = Ritning()
-annanHus.Repaint("Red")
-print(ayadsHus.GetColor())
-print(annanHus.GetColor())
-##########################################
+stefansHus = Ritning()
+stefansHus.Repaint("blue")
+annasHus = Ritning()
+annasHus.Repaint("Red")
+print(stefansHus.GetColor())
+print(annasHus.GetColor())
+
+#######################################
+class Student:
+    def __init__(self, namn:str, age:int):#constutior #*
+        #skapa alla propertties/Engenskaper/varibler
+        #som hör till ritningen
+        self.Namn = namn #* 
+        self.__Age = 0 #* #priviate/protected variabel
+        self.SetAge(age) #sätter age i början genom att anrompa metoden SetAge
+        self.__postnr = "" #* #properties bäst och köra __ på allt
+        self.Kurslista = []
+
+    def SetAge(self, newAge:int): #för att ändra på __Age
+        if newAge > 0:
+            self.__Age = newAge
+    def GetAge(self):
+        return self.__Age
+
+stefan = Student("Stefan", -12) #skapar en student
+stefan.SetAge(-13)
+#stefan.SetAge(132)
+print(stefan.GetAge())
 
 
-
-# class Student:
-#     def __init__(self, namn:str, age:int):
-#         #skapa alla propertties/Engenskaper/varibler
-#         #som hör till ritningen
-#         self.Namn = namn 
-#         #self.Age = age
-#         self.__Age = age #priviate/protected variabel
-#         self.__postnr = "" #properties bäst och köra __ på allt
-#         self.Kurslista = []
-
-#     def SetAge(self, newAge:int): #för att ändra på __Age
-#         if newAge > 0:
-#             self.__Age = newAge
-
-# stefan = Student("Stefan", -12) #skapar en student
-# stefan.SetAge(-13)
-# stefan.SetAge(132)
-# #print(stefan.__Age)
-# stefan.Age = -123 #för att sätta värde
-
-# print(stefan.Namn, stefan.Age)
-# #print(stefan.__dict__) #varje object ligger som dict
+print(stefan.Namn, stefan.Age)
+#print(stefan.__dict__) #varje object ligger som dict
 
 # #Guide för att skapa en class
 # # 1. class Namn
@@ -76,6 +99,20 @@ print(annanHus.GetColor())
 # #         här skapar du properties för objecktet
 # # 3. __ på alla propeties
 # # 4. getter och setters med Vaildering
-# # 5. constuctor - vad skicka in
+# # 5. constuctor - vad skicka in -vi måste definera vad är vaild state
 
-# #lektion 1:24 min
+# #lektion 1:24 min lektion 20221003_083217
+
+#construtor parameterar vad krävs för att objekten ska vara vallied
+#måste lägga de parametar som ibörjan måste vara satta för att objectet ska var vaild
+
+class Person:
+    def __init__(self, personnr:str ): 
+        self.__Name = ""
+        self.__Personnr = personnr
+    def Setpersonnr(self, new:str): #sett personnummer
+        self.__Personnr = new
+    def Setname(self, new:str): #sett namn
+        self.__Name = new
+
+s = Person("21324213-32123")
