6691ab31e762313939af962ec3ffd8ddd74a6ac3
Fr2dX43 <a_a93@live.se>
2022-11-05 22:20:02 +0100

    w6

--- w6/person.py
+++ w6/person.py
@@ -1,5 +1,17 @@
 class Person:
     def __init__(self, name:str): #anvönd konstruktor på tentan
-        self.__name = name
+        self.__Name = name
         self.__Presents = []
 
+    def AddChrismasPresent(self, present):
+        self.__Presents.append(present)
+
+
+    def GetChrismasPresentTotal(self):
+        sum = 0
+        for person in self.__Presents:
+            sum = sum + person.GetPrice()
+        return sum
+
+    def GetName(self):
+        return self.__Name
